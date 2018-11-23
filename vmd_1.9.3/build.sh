bc443825f48a9e00b57d42b2edb9c5ed035f1994
Alex Sha <alex@intbio.org>
2018-11-23 18:41:29 +0300

    Fri Nov 23 18:41:29 MSK 2018

--- vmd_1.9.3/build.sh
+++ vmd_1.9.3/build.sh
@@ -10,58 +10,55 @@ if [[ $target_platform == osx* ]]; then
 
 #export DYLD_LIBRARY_PATH=/opt/MacOSX10.9.sdk/usr/lib/system/:$DYLD_LIBRARY_PATH
 export MACOSX_DEPLOYMENT_TARGET=10.9 # - helps on some OSX platforms
-#Compile plugins first
-export PLUGINDIR="$(pwd)/vmd-1.9.3/plugins"
+export OSX_VER=10.9
+
+cd vmd-1.9.3/lib/fltk
+
+wget http://fltk.org/pub/fltk/snapshots/fltk-1.4.x-r13117.tar.gz
+tar -xf fltk-1.4.x-r13117.tar.gz
+
+ln -s fltk-1.4.x-r13117 fltk
+
+ln -s fltk include
+cd fltk
+./configure --prefix="$PWD/../MACOSXX86_64" --exec-prefix="$PWD/../MACOSXX86_64" --libdir="$PWD/../MACOSXX86_64" CXXFLAGS="-mmacosx-version-min=$OSX_VER" LDFLAGS="-mmacosx-version-min=$OSX_VER"
+make -j 8
+make install
+
+
+cd ..
+cd ..
+cd ..
+cd ..
+
+export PLUGINDIR="$PWD/vmd-1.9.3/plugins"
+export export TCLINC=-I/System/Library/Frameworks/Tcl.framework/Versions/8.5/Headers
+export export TCLLIB=-L/System/Library/Frameworks/Tcl.framework/Versions/8.5/Headers
 cd plugins
-export export TCLINC=-I$PREFIX/include/
-export export TCLLIB=-L$PREFIX/lib/
 make   MACOSXX86_64 TCLINC=$TCLINC TCLLIB=$TCLLIB
 make   distrib 
 cd ../vmd-1.9.3
-# echo "MACOSXX86_64 LP64 FLTKOPENGL FLTK TK TDCONNEXION LIBTACHYON NETCDF TCL PYTHON PTHREADS NUMPY ACTC GCC" > configure.options
-# echo "MACOSXX86_64 LP64  FLTK FLTKOPENGL TK  NETCDF TCL PTHREADS  GCC PYTHON PTHREADS NUMPY" > configure.options
-# echo "MACOSXX86_64 LP64  FLTK OPENGL TK  NETCDF TCL PTHREADS  GCC " > configure.options
 
-#This compiles but no graphics.
-echo "MACOSXX86_64 LP64  FLTK  TK  NETCDF TCL PTHREADS  GCC " > configure.options
-
+echo "MACOSXX86_64 LP64 FLTKOPENGL FLTK TK  TCL PTHREADS " > configure.options
 
 #Fix code
 sed -i.bak 's/MACOSX/MACOSXX86/g' bin/vmd.sh
 sed -i.bak 's/MACOSX/MACOSXX86/g' bin/vmd.csh
-# sed -i.bak 's/__APPLE__/__APPLE__NO/g' src/VMDTkinterMenu.h
-# sed -i.bak 's/__APPLE__/__APPLE__NO/g' src/PythonTextInterp.h
-# sed -i.bak 's/__APPLE__/__APPLE__NO/g' src/PythonTextInterp.C
-# sed -i.bak 's/__APPLE__/__APPLE__NO/g' src/py_commands.h
+
 
 #export VMDINSTALLNAME='vmd'
 export VMDINSTALLBINDIR=$PREFIX/bin #/usr/local/bin
 export VMDINSTALLLIBRARYDIR=$PREFIX/vmd #/usr/local/lib/$install_name
-export PYTHON_INCLUDE_DIR=$PREFIX/include/python2.7
-export PYTHON_LIBRARY_DIR=$PREFIX/lib/python2.7/config
-export NUMPY_INCLUDE_DIR=$PREFIX/lib/python2.7/site-packages/numpy/core/include
-export NUMPY_LIBRARY_DIR=$PREFIX/lib/python2.7/site-packages/numpy/core/include
-export TCL_INCLUDE_DIR=$PREFIX/include/
-export TCL_LIBRARY_DIR=$PREFIX/lib/
-
-#fix code
-# sed -i.bak 's/lpython2.5/lpython2.7/g' configure
-# sed -i.bak 's/$python_include     = "-F/#/g' configure
-# sed -i.bak 's/$python_library     = "";/#/g' configure
-# sed -i.bak 's/$python_libs        = "-framework Python -lpthread";/#/g' configure
-
+# export PYTHON_INCLUDE_DIR=$PREFIX/include/python2.7
+# export PYTHON_LIBRARY=$PREFIX/lib/python2.7/config
+# export TCL_INCLUDE_DIR=$PREFIX/include/
+# export TCL_LIBRARY_DIR=$PREFIX/lib/
 
 ./configure 
 cd src
-#fix Makefile
-sed -i.bak 's/tk8.5-x11/tk8.5/g' Makefile
-sed -i.bak 's/tcl8.5-x11/tcl8.5/g' Makefile
-sed -i.bak 's/lfltk-x11/lfltk/g' Makefile
-sed -i.bak 's/-lnetcdf/ /g' Makefile
-sed -i.bak 's/-lfltk_gl/ /g' Makefile
-sed -i.bak "s&-L../lib/fltk/MACOSXX86_64&-L$TCL_LIBRARY_DIR&g" Makefile
-
-
+sed -i.bak 's/fltk-1.3.x/fltk/g' Makefile
+sed -i.bak 's%../lib/tk/lib_MACOSXX86_64/Tk.framework/Versions/8.5/Headers%/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers%g' Makefile
+make veryclean
 make -j 8
 make install
 ## See INSTALL of gromacs distro
